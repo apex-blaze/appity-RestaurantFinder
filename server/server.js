9601d2d7061b31d6284363aa70ee12e30c83db70
Ritish <ritish619@gmail.com>
2021-02-13 15:32:08 +0530

    Added pg on backend

--- server/server.js
+++ server/server.js
@@ -1,26 +1,66 @@
 require("dotenv").config();
 const express = require("express");
+// const morgan = require("morgan");
+const db = require("./db");
 
 const app = express();
+app.use(express.json());
+// app.use(morgan("tiny"));
 
 // Get  all restaurants
-app.get("/api/v1/restaurants", (req, res) => {
-  res.status(200).json({
-    status: "Success",
-    data: {
-      restaurants: ["Pizza Hut", "KFC", "McDownalds"],
-    },
-  });
+app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM restaurants");
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Get a restaurant
-app.get("/api/v1/restaurant/:id", (req, res) => {
-  console.log(req.params);
+app.get("/api/v1/restaurants/:id", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "Wendys",
+    },
+  });
 });
 
 // Create a restaurant
 app.post("/api/v1/restaurants", (req, res) => {
   console.log(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      restaurant: "Wendys",
+    },
+  });
+});
+
+// Update restaurant
+app.put("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params.id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "Wendys",
+    },
+  });
+});
+
+// Delete restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+  });
 });
 
 const port = process.env.PORT || 5050;
